233385143774ffe0462462e3789f4440e8c123aa
killonifi <106478835+killonifi@users.noreply.github.com>
2025-08-07 11:54:47 +0300

    Implement forward and bridge topologies and improve GUI

--- converter_tool/calculations/__init__.py
+++ converter_tool/calculations/__init__.py
@@ -1,11 +1,13 @@
 from .flyback_design import FlybackDesign
 from .forward_design import ForwardDesign
+from .push_pull_design import PushPullDesign
 from .half_bridge_design import HalfBridgeDesign
 from .full_bridge_design import FullBridgeDesign
 
 __all__ = [
     "FlybackDesign",
     "ForwardDesign",
+    "PushPullDesign",
     "HalfBridgeDesign",
     "FullBridgeDesign",
 ]
